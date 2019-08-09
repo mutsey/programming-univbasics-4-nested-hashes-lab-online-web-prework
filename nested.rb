49c1b5db896cad7e01a775d05344450629a6fd3c
Meagan Utsey <meagan.utsey@gmail.com>
2019-08-09 05:48:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,10 +1,9 @@
-
 def hopper
 	programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+          :languages => ["COBOL", "FORTRAN"]}
         },
         :alan_kay => {
           :known_for => "Object Orientation",
